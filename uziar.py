55cc149bbcecb198880d72b919ef02dc4a8cf0b2
ARHAM ALI <157973586+ARHAMALI0@users.noreply.github.com>
2024-02-23 13:03:47 -0800

    Update uziar.py

--- uziar.py
+++ uziar.py
@@ -1,8 +1,6 @@
+#button example
 import streamlit as st
 
-def main():
-    st.title("Food Ordering Website")
-    st.markdown(open("index.html").read(), unsafe_allow_html=True)
-
-if __name__ == "__main__":
-    main()
+# Button example
+if st.button('Click me!'):
+    st.write('Button clicked!')
