e3a5742b41f07bde1a35645a9cead878e9f86bc6
ARHAM ALI <157973586+ARHAMALI0@users.noreply.github.com>
2024-02-23 12:54:58 -0800

    Update uziar.py

--- uziar.py
+++ uziar.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 def main():
-    st.title("Food Ordering Website")
+    st.title("Food Ordering Website"
     st.markdown(open("index.html").read(), unsafe_allow_html=True)
 
 if __name__ == "__main__":
