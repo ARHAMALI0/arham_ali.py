133d4494f657c5fc096fc31159fcede81a01d991
ARHAM ALI <157973586+ARHAMALI0@users.noreply.github.com>
2024-02-23 13:11:06 -0800

    Update 2 arham.py

--- 2 arham.py
+++ 2 arham.py
@@ -1,18 +1,5 @@
 import streamlit as st
 
-# Display raw HTML
-st.write("""
-    <h1 style='color: blue;'>This is HTML</h1>
-    <button>This is a button</button>
-""")
-
-# You can also use the `st.markdown` function to render HTML
-st.markdown("""
-    <h1 style='color: green;'>This is Markdown with HTML</h1>
-    <button>This is a button</button>
-""", unsafe_allow_html=True)
-import streamlit as st
-
 # Read HTML file
 with open('path/to/your/html/file.html', 'r') as f:
     html_content = f.read()
